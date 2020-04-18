bbcae783a8aac842a3ca271249d0a019401972c4
asanchez412 <sanchezandres41997@gmail.com>
2020-04-17 21:03:11 -0300

    v1 - Se agregan dos tests

--- src/Test/Library.Test/DemoTest.cs
+++ src/Test/Library.Test/DemoTest.cs
@@ -1,22 +1,45 @@
 using NUnit.Framework;
+using Full_GRASP_And_SOLID.Library;
 
 namespace Library.Test
 {
+    [TestFixture]
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        [TestCase]
+        public void GetStepCost()
         {
-            // Insertá tu código de inicialización aquí
+            Recipe recipe = new Recipe();
+            Equipment equipment = new Equipment("Cafetera", 5);
+            Product product = new Product("Café", 1);
+            Step step = new Step(product, 10, equipment, 120);
+            
+            double prueba_GetStepCost() // Cambiá el nombre para indicar qué estás probando
+            {
+                double prueba = (step.Input.UnitCost * step.Quantity) + (step.Equipment.HourlyCost * step.Time);
+                return prueba;
+            }   
+                Assert.AreEqual(prueba_GetStepCost(), 610);
         }
-
-        [Test]
-        public void DemoTest() // Cambiá el nombre para indicar qué estás probando
+        [TestCase]
+        public void GetProductionCost()
         {
-            int a = 1;
-            int b = 2;
+            Recipe recipe = new Recipe();
+            Equipment equipment = new Equipment("Cafetera", 5);
+            Product product = new Product("Café", 1);
+            Step step = new Step(product, 10, equipment, 120);
+            Step step1 = new Step(product, 25, equipment, 90);
 
-            Assert.AreEqual(a + b, 3);
+            double prueba_GetProductionCost()
+            {
+                double prueba = 0;
+                foreach (Step step in recipe.steps)
+            {
+                prueba = prueba + step.GetStepCost();
+            }
+            return prueba;
+            }
+            Assert.AreEqual(prueba_GetProductionCost(), 100);
         }
     }
 }
